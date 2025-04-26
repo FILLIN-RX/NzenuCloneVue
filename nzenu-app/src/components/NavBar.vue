23f20d0acf2798f2b31b8e27c0c862b940661730
RUXEL <djeutchouruxel@gmail.com>
2025-04-26 15:11:33 +0100

    facture

---
+++ nzenu-app/src/components/NavBar.vue
@@ -0,0 +1,45 @@
+
+
+<template>
+    <nav class="w-100 p-2 bg-primary">
+        <div class="row">
+            <div class="col">
+                <div class="col">1
+                    <div class="row">
+                        <div class="col">
+                            <router-link class="nav-link" to="/"><img src="../assets/logo_Nzenuhost1_copie_2_4x-8_rnfqnu.png" alt=""></router-link>
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <div class="col">
+                                    <p>Avis client</p>
+                                </div>
+                                <div class="col"></div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+
+    </nav>
+
+
+</template>
+
+<script>
+
+
+
+
+</script>
+
+
+<style>
+
+
+</style>
