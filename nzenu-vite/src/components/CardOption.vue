adf7fc68c35fc8131ce6e48330a3bb7ce803b467
RUXEL <djeutchouruxel@gmail.com>
2025-06-12 20:29:30 +0100

    creating the profil component and correcting the svg problem

--- nzenu-vite/src/components/CardOption.vue
+++ nzenu-vite/src/components/CardOption.vue
@@ -1,33 +1,25 @@
-
-
 <template>
-  <div class=" p-3 space-y-2 h-full">
+  <div class="p-3 space-y-2 h-full">
     <div class="flex space-x-3">
-      <div class="col-1 icon " v-html="icon"></div> <!-- Injection du SVG -->
-      <div class="col title font-bold  mb-1">{{ title }}</div>
+      <div class="col-1 icon">
+        <slot name="icon" />
+      </div>
+      <div class="col title font-bold mb-1">{{ title }}</div>
     </div>
-    
+
     <p class="content">{{ content }}</p>
   </div>
 </template>
 
 <script setup>
-
-
 defineProps({
-  icon: String,   
   title: String,
   content: String,
-
-  
 });
 </script>
 
 <style scoped>
-
 .icon {
   margin-bottom: 10px;
 }
-
-
 </style>
